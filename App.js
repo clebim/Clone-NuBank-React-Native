70ab0554ee4a604bc114ff6e1a7325bac900623a
Matheus Cleber <37844991+clebim@users.noreply.github.com>
2020-03-23 14:54:56 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Main from './src/pages/index'; 
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+      <Main />
+    </>  
+  );
+}
+
